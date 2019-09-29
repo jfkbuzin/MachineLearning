bb6ce0e7e3e988f9d2789eb89ed758f83b3df427
Guilherme Delazeri <delazeri1997@gmail.com>
2019-09-29 15:31:08 -0300

    Inclusão da função para calcular o F-Score

--- FlorestaPy/Util.py
+++ FlorestaPy/Util.py
@@ -1,5 +1,29 @@
 import math
 
-
 def log2(x):
     return math.log(x) / math.log(2)
+
+def prec(vp, fp):
+    return  vp / (vp + fp)
+
+def rec(vp, fn):
+    return  vp / (vp + fn)
+
+def fScore(listOfTuples, beta):
+    vp = 0
+    fp = 0
+    fn = 0
+    vn = 0
+    for tup in listOfTuples:
+        if tup[0] == 'Sim' and tup[1] == 'Sim':
+           vp += 1
+        elif tup[0] == 'Sim' and tup[1] == 'Nao':
+           fn += 1
+        elif tup[0] == 'Nao' and tup[1] == 'Sim':
+           fp += 1
+        else:
+           vn += 1
+    precision = prec(vp, fp)
+    recall = rec(vp, fn)
+
+    return (1 + beta**2) * ((precision * recall) / ((beta**2 * precision) + recall))
